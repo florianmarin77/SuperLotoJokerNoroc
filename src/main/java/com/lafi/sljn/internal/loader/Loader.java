3088947407b2e4a865fbfdf5535f97065a66e19a
florianmarin77 <elCostadelRica#77>
2022-11-17 08:49:05 +0200

    Add documentation comment for Loader interface.

--- src/main/java/com/lafi/sljn/internal/loader/Loader.java
+++ src/main/java/com/lafi/sljn/internal/loader/Loader.java
@@ -4,6 +4,13 @@ import java.nio.file.Path;
 import java.util.List;
 
 public interface Loader<T> {
+
+    /**
+     * Loads all objects as sample type or single type.
+     *
+     * @param path the location of the text resource
+     * @return a list of samples or a list of singles
+     */
     default List<T> loadData(Path path) {
         return null;
     }
